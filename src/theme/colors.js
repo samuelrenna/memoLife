760513871ac2ce0569a5bbb250f97c3f00363f2f
Samuel Renna Gamboa <samuelrenna@gmail.com>
2024-07-20 21:19:03 +0200

    estructura de proyecto

---
+++ src/theme/colors.js
@@ -0,0 +1,13 @@
+// colores para la aplicación
+const colors = {
+    primaryBackground: '#FFFFFF', // Fondo Principal: Blanco
+    secondaryBackground: '#F1F1F1', // Fondo Secundario: Gris Claro
+    primaryText: '#000000', // Texto Principal: Negro
+    secondaryText: '#B0B0B0', // Texto Secundario: Gris Medio
+    buttonLink: '#3A77FF', // Botones y Enlaces: Azul Suave
+    highlightNotification: '#FFD700', // Resaltados y Notificaciones: Amarillo Suave
+    positiveAction: '#3DDC84', // Acciones Positivas: Verde Suave
+    alertError: '#FF6B6B', // Alertas y Errores: Rojo Suave
+};
+
+export default colors;
